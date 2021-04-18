e7a6853b3da771d7599c791c5d5dfdec212ec7ad
DBohlke <78039823+DBohlke@users.noreply.github.com>
2021-04-17 19:31:49 -0500

    Added function to convert temperatures

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ let form = document.querySelector("#search-input");
 form.addEventListener("submit", handleSubmit);
 
 function showWeather(response) {
-  console.log(response.data);
   document.querySelector("#city-state").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML =
-    Math.round(response.data.main.temp) + "°F";
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
-  document.querySelector("#max-temp").innerHTML =
-    Math.round(response.data.main.temp_max) + "°F";
-  document.querySelector("#min-temp").innerHTML =
-    Math.round(response.data.main.temp_min) + "°F";
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#min-temp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
   document.querySelector("#wind").innerHTML =
     "Wind: " + Math.round(response.data.wind.speed) + "mph";
   document.querySelector("#real-feel").innerHTML =
-    "Feels like: " + Math.round(response.data.main.feels_like) + "°F";
+    "Feels like: " + Math.round(response.data.main.feels_like);
 
   let iconElement = document.querySelector("#icon");
 
@@ -96,10 +98,97 @@ if (minutes < 10) {
 let ampm = "am";
 if (hours > 12) {
   hours -= 12;
-  ampm = "am";
+  ampm = "pm";
 }
 
 date.innerHTML = `${currentDay}, ${currentMonth} ${currentDate}`;
 time.innerHTML = `${hours}:${minutes}${ampm}`;
+
+let temperatureInFahrenheit = null;
+let temperatureInCelsius = null;
+
+let maxInFahrenheit = [];
+let minInFahrenheit = [];
+
+let maxInCelsius = [];
+let minInCelsius = [];
+
+let realFeelFahrenheit = null;
+let realFeelCelsius = null;
+
+let maxTemperatureElement = "";
+let minTemperatureElement = "";
+
+let maxUnitElement = "";
+let minUnitElement = "";
+
+function changeUnit(event) {
+  event.preventDefault();
+  let currentUnit = document.querySelector("#unit");
+  if (currentUnit.innerHTML === "°F") {
+    let celsiusTemperature = Math.round(
+      ((temperatureInFahrenheit - 32) * 5) / 9
+    );
+    let celsiusTemperatureElement = document.querySelector("#temperature");
+    let maxTemperature = Math.round(((maxInFahrenheit - 32) * 5) / 9);
+    let maxTemperatureElement = document.querySelector("#max-temp");
+    let minTemperature = Math.round(((minInFahrenheit - 32) * 5) / 9);
+    let minTemperatureElement = document.querySelector("#min-temp");
+    let realFeelTemperature = Math.round(((realFeelFahrenheit - 32) * 5) / 9);
+    let realFeelTemperatureElement = document.querySelector("#real-feel");
+    let unitElement = document.querySelector("#unit");
+    let maxUnitElement = document.querySelector("#max-unit");
+    let minUnitElement = document.querySelector("#min-unit");
+    let unitConvertElement = document.querySelector("#unit-link");
+    let realFeelUnitElement = document.querySelector("#real-feel-unit");
+
+    temperatureInCelcius = celsiusTemperature;
+    maxInCelsius = maxTemperature;
+    minInCelsius = minTemperature;
+    realFeelCelsius = realFeelTemperature;
+    celsiusTemperatureElement.innerHTML = celsiusTemperature;
+    maxTemperatureElement.innerHTML = maxTemperature;
+    minTemperatureElement.innerHTML = minTemperature;
+    realFeelTemperatureElement.innerHTML = realFeelTemperature;
+
+    unitElement.innerHTML = "°C";
+    maxUnitElement.innerHTML = "°C";
+    minUnitElement.innerHTML = "°C";
+    unitConvertElement.innerHTML = "°F";
+    realFeelUnitElement.innerHTML = "°C";
+
+  }else{
+
+        let fahrenheitTemperature = Math.round(
+          ((temperatureInCelcius * 9) / 5) + 32
+        );
+        let fahrenheitTemperatureElement = document.querySelector("#temperature");
+        let maxTemperature = Math.round(((maxInCelsius * 9) / 5) + 32);
+        let maxTemperatureElement = document.querySelector("#max-temp");
+        let minTemperature = Math.round(((minInCelsius * 9) / 5) + 32);
+        let minTemperatureElement = document.querySelector("#min-temp");
+        let realFeelTemperature = Math.round(((realFeelCelsius * 9) / 5) + 32);
+        let realFeelTemperatureElement = document.querySelector("#real-feel");
+        let unitElement = document.querySelector("#unit");
+        let maxUnitElement = document.querySelector("#max-unit");
+        let minUnitElement = document.querySelector("#min-unit");
+        let unitConvertElement = document.querySelector("#unit-link");
+        let realFeelUnitElement = document.querySelector("#real-feel-unit");
+
+        fahrenheitTemperatureElement.innerHTML = fahrenheitTemperature;
+        maxTemperatureElement.innerHTML = maxTemperature;
+        minTemperatureElement.innerHTML = minTemperature;
+        realFeelTemperatureElement.innerHTML = realFeelTemperature;
+
+        unitElement.innerHTML = "°F";
+        maxUnitElement.innerHTML = "°F";
+        minUnitElement.innerHTML = "°F";
+        unitConvertElement.innerHTML = "°C";
+        realFeelUnitElement.innerHTML = "°F";
+  }
+}
+
+let unitElement = document.querySelector("#unit-convert");
+unitElement.addEventListener("click", changeUnit);
 
 searchCity("55901");
